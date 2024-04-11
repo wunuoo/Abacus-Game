9aee1a4a62b15af46fabff13bcf6f9b873357850
wunuoo <xuwunuo@foxmail.com>
2024-04-11 14:42:34 +0800

    Revert "Merge branch 'main' into save"
    This reverts commit cdd9faecba94f3652a5903c921753e87173549e8, reversing
    changes made to 5343a78ff8a643404d09b3fe75dd3ac51e3f6570.

--- Assets/Scripts/Objects/Bead.cs
+++ Assets/Scripts/Objects/Bead.cs
@@ -3,22 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[Serializable]
 public class Bead : MonoBehaviour
 {
     public static float height = 0.1f;
     public int index;
     public Shaft owner;
     bool selected;
-    Vector2 slotPos;//算珠目前停靠的位置
-
     public bool isDown = true;//在下面是true
-
-    public void Start()
-    {
-        slotPos = transform.position;
-    }
-
+    
     public void OnMouseOver()//跟随鼠标
     {
         if (Input.GetMouseButtonDown(0))
@@ -55,22 +48,6 @@ public class Bead : MonoBehaviour
             //transform.position = new Vector2(transform.position.x, transform.position.y + deltaPosY);
             
         }
-    }
-
-    public void ChangeSlot(float maxDistance)
-    {
-        this.isDown = !isDown;//若之前算珠在下，现在isdown应该是false =》 此次位移是往上
-        slotPos += new Vector2(0, isDown ? -maxDistance : maxDistance);//当isDown为true，说明此次位移是往下
-    }
-
-    public void MoveToSlot()
-    {
-        transform.position = slotPos;
-    }
-
-    public void MoveTo(Vector2 Destination)
-    {
-        transform.position = Destination;
     }
 }
 
