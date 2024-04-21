33c52c68b469e3adf58ad5bc69548f179eaad244
wunuoo <xuwunuo@foxmail.com>
2024-04-22 01:30:12 +0800

    Tools_Changed_20240421
    新增了道具面板

---
+++ Assets/Scripts/Objects/ToolInfo.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(menuName = "创建工具信息", fileName = "工具")]
+public class ToolInfo : ScriptableObject
+{
+    [Header("工具名称")]
+    public string toolName;
+
+    [TextArea, Header("工具信息")]
+    public string toolDescription;
+    
+    [Header("工具图片")]
+    public Sprite toolImage;
+}
+
+
---
+++ Assets/Scripts/UI/UITools.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITools : MonoBehaviour
+{
+    public GameObject descriptionPanel;
+    public TextMeshProUGUI toolName;
+    public TextMeshProUGUI toolDescription;
+    public Image toolImage;
+
+
+    public void ShowDescription(int index)
+    {
+        ToolInfo toolInfo = ToolInfoManager.Instance.tools[index];
+        toolName.text = toolInfo.toolName;
+        toolDescription.text = "<rotate=90>" + toolInfo.toolDescription;
+        toolImage.sprite = toolInfo.toolImage;
+        toolImage.SetNativeSize();
+        descriptionPanel.SetActive(true);
+    }
+
+    public void HideDescription()
+    {
+        descriptionPanel.SetActive(false);
+    }
+
+    public void OnClickTool(int index)
+    {
+        ShowDescription(index);
+
+    }
+
+}
